79a407fb70f26f2e099204896ba5f5f710043883
Hashir Khan <hkhan6265@bths.edu>
2021-02-23 20:07:05 -0500

    Finished login and logout

---
+++ Twitter/tweetViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  tweetViewCell.swift
+//  Twitter
+//
+//  Created by Hashir Khan on 2/23/21.
+//  Copyright © 2021 Dan. All rights reserved.
+//
+
+import UIKit
+
+class tweetViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
