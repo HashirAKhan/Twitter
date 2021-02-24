88d97ed83f3193a51ac656f62020bfaeee61c6ed
Hashir Khan <hkhan6265@bths.edu>
2021-02-23 20:45:56 -0500

    completed api call

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -9,11 +9,29 @@
 import UIKit
 
 class HomeTableViewController: UITableViewController {
+    var tweetArray = [NSDictionary]()
+    var numberOfTweet: Int!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        getTweets()
 
-      
+    }
+    
+    func getTweets(){
+        let url = "https://api.twitter.com/1.1/statuses/home_timeline.json"
+        let tweetParams = ["count": 10]
+        TwitterAPICaller.client?.getDictionariesRequest(url: url, parameters: tweetParams, success: { (tweets: [NSDictionary]) in
+            self.tweetArray.removeAll()
+            for tweet in tweets {
+                self.tweetArray.append(tweet)
+            }
+            
+            self.tableView.reloadData()
+        }, failure: { (Error) in
+            print("Error fetching Tweets")
+        })
     }
 
     // MARK: - Table view data source
@@ -25,7 +43,7 @@ class HomeTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 5
+        return tweetArray.count
     }
 
     @IBAction func logoutPress(_ sender: Any) {
@@ -36,6 +54,10 @@ class HomeTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "tweetCell", for: indexPath) as! TweetViewCell
+        let user = tweetArray[indexPath.row]["user"] as! NSDictionary
+        
+        cell.userName.text = user["name"] as? String
+        cell.tweetText.text = tweetArray[indexPath.row]["text"] as? String
         return cell
     }
 }
